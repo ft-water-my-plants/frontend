9346ff0e66fa1a07346889f6521e6295c27d6d85
Ryan Mark <rymark907@gmail.com>
2021-09-20 11:19:46 -0700

    Adding to signup component, created a validation schema for signup component

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,29 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Signup() {
+export default function Signup(props) {
 
+    const { /* props */ } = props;
+
+    // initial form values
+    const initialSignUpValues = {
+        username: "",
+        phoneNumber: "",
+        password: "",
+    };
+
+    // Setting state for form values
+    const [signUpValues, setSignUpValues] = useState(initialSignUpValues);
+
+    // Functions for updating form state
+    const onChange = (name, value) => {
+        setSignUpValues({ ...signUpValues, [name]: value});
+    }
+
+    const change = (event) => {
+        const { name, value } = event.target;
+        onChange(name, value);
+    }
+
+    // Functions for submitting changes
+
+    const submitChange = (event) => {
+        event.preventDefault();
+
+    }
 
     return(
         <div>
-            <form>
+            <form onSubmit={submitChange}>
+                <div className="errors">
+                    
+                </div>
                 <label> Username:
                     <input 
                         type="text"
                         name="username"
+                        onChange={change}
                         />
                 </label>
                 <label> Phone Number:
                     <input 
                         type="text"
-                        name="phonenumber"
+                        name="phoneNumber"
+                        onChange={change}
                         />
                 </label>
                 <label> Password:
                     <input 
                         type="password"
                         name="password"
+                        onChange={change}
                         />
                 </label>
+                <button>SignUp!</button>
             </form>
         </div>
     )
