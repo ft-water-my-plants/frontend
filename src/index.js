ead9505f2de603169934531dd911783a8e11e99e
Nathan <nathan.forman@gmail.com>
2021-09-20 10:05:16 -0700

    Filled out App and src index also installed redux and more

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducer from './reducers/index';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
