768faae4a2a99cc3fd027c1685fa7ccb7b23c3e9
peterdavidconley <85258866+peterdavidconley@users.noreply.github.com>
2021-09-20 11:02:39 -0400

    created plant object with initial form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Plant from './PlantObject'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>Water my Plants</h1>
+        <Plant />
+      </center>
     </div>
   );
 }
