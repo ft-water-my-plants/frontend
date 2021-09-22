e7b9ed4b4f2f6bfc0f5ff98ab6c028dd6b8d1614
Peter Conley <peterdavidconley@gmail.com>
2021-09-22 13:18:51 -0400

    created plantcard js file, added css class

---
+++ src/components/PlantCard.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+import * as yup from 'yup'
+import axios from 'axios'
+
+function PlantCard () {
+
+    return (
+        <div className='plantcard'>
+
+        </div>
+    )
+}
+
+export default PlantCard
