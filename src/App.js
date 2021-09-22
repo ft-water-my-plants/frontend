dd838da4ef925f20e8818bc1f9cf3a51802dd244
Nathan <nathan.forman@gmail.com>
2021-09-22 13:34:07 -0700

    Modal created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Plant from './components/PlantObject';
 import Signup from './components/signup';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
-import PlantCard from './components/PlantCard'
+import PlantCard from './components/PlantCard';
+import UpdateUser from './components/UpdateUser';
 
 // import * as yup from 'yup';
 
 import './App.css';
-import UpdateUser from './components/UpdateUser';
+import axios from 'axios';
 
 function App() {
+  const [plant, setPlant] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://water-my-plants-bw3.herokuapp.com/api/plants')
+    .then(res => {
+      setPlant(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, []);
+
   return (
     <Router>
       <div className='App'>
@@ -20,15 +33,17 @@ function App() {
             <h2>Water my Plants</h2>
           </div>
           <ul className="links-list">
-            <li>
-              <Link className="link" to='/user'>Update User</Link>
-            </li>
+            
             <li>
               <Link className="link" to='/plants'>Plants</Link>
             </li>
             <li>
+              <Link className="link" to='/user'>Update User</Link>
+            </li>
+            <li>
               <Link className="link" to='/signup'>New Account</Link>
             </li>
+            
             <li>
               <Link className="link" to='/login'>Login</Link>
             </li>
@@ -38,7 +53,11 @@ function App() {
           <center>
             <h1>Water my Plants</h1>
             <Route path='/plant-card' component={PlantCard} />
-            <PrivateRoute path='/plants' component={Plant} />
+            <PrivateRoute 
+              path='/plants' 
+              component={Plant} 
+              render={props => <PlantCard {...props} setPlant={setPlant}/>}
+            />
             <PrivateRoute path='/user' component={UpdateUser} />
             <Route path='/signup' component={Signup} />
             <Route path='/login' component={Login}/>
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,20 +2,59 @@ import React, { useEffect, useState } from 'react'
 import '../App.css';
 import * as yup from 'yup'
 import axios from 'axios'
+import Modal from './Modal';
 
-function PlantCard () {
+function PlantCard (props) {
+    const [plant, setPlant] = useState({});
+    const [showModal, setShowModal] = useState(false);
+
+    const { id } = props.match.params;
+
+    useEffect(() => {
+        axios.get(`https://water-my-plants-bw3.herokuapp.com/api/plants/${id}`)
+            .then(res => {
+                setPlant(res.data);
+            });
+    }, []);
+
+    if (!plant){
+        return <h3>Loading plant...</h3>;
+    }
+
+    const handleEdit = () => {
+        props.history.push(`/plants/${plant.id}`);
+    }
+
+    const handleDelete = () => {
+        setShowModal(true);
+    }
+
+    const handleYesFunc = () => {
+        axios.delete(`https://water-my-plants-bw3.herokuapp.com/api/plants/${plant.id}`)
+        .then(res => {
+            props.setPlant(res.data);
+            props.history.push('/plants');
+        })
+    }
+
+    const handleNoFunc = () => {
+        setShowModal(false);
+    }
 
     return (
         <div className='plantcard'>
             <div>
-                <h3>Plant Nickname: (nickname variable) </h3>
-                <h3>Species: (species variable) </h3>
-                <h3>Water Frequency: (water variable) </h3>
+                <h3>Plant Nickname: {plant.nickname} </h3>
+                <h3>Species: {plant.species} </h3>
+                <h3>Water Frequency: {plant.water} </h3>
             </div>
             <div>
                 <center>
-                <button>Edit</button>
-                <button>Delete</button>
+                <button onClick={handleEdit}>Edit</button>
+                <button onClick={handleDelete}>Delete</button>
+                {
+                    showModal && (<Modal title={`Are you sure you want to delete ${plant.nickname}?`} yesFunc={handleYesFunc} noFunc={handleNoFunc}/>)
+                }
                 </center>
             </div>
         </div>
